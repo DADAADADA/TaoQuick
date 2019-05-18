163d244fe65bdda162fed1717f0e12514d7ccb30
jiawentao <wentaojia2014@163.com>
2019-05-19 02:32:32 +0800

    CI: update macos/deploy.sh

--- scripts/macos/deploy.sh
+++ scripts/macos/deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-/usr/local/opt/qt/bin/macdeployqt bin/TaoQuickDemo.app -qmldir=/usr/local/opt/qt/qml -verbose=1 -dmg
+mkdir bin/TaoQuick bin/TaoQuickDemo.app/Contents/Resources/qml
 cp -R bin/TaoQuick bin/TaoQuickDemo.app/Contents/Resources/qml/TaoQuick
+/usr/local/opt/qt/bin/macdeployqt bin/TaoQuickDemo.app -qmldir=/usr/local/opt/qt/qml -verbose=1 -dmg
 mv bin/TaoQuickDemo.dmg bin/TaoQuickDemo_macos10-14_xcode10-2.dmg
