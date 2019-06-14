9253d938a143aca7e331da77a038ecc85dad5818
jaredtao <wentaojia2014@163.com>
2019-06-15 00:20:37 +0800

    update ci

--- scripts/ubuntu/deploy.sh
+++
@@ -1,6 +0,0 @@
-﻿#! /bin/bash
-wget https://github.com/probonopd/linuxdeployqt/releases/download/6/linuxdeployqt-6-x86_64.AppImage
-mv linuxdeployqt-6-x86_64.AppImage linuxdeployqt
-chmod a+x linuxdeployqt
-./linuxdeployqt bin/TaoQuickDemo -qmake="/opt/qt512/bin/qmake" -qmldir="/opt/qt512/qml" -appimage
-move bin/TaoQuickDemo.AppImage bin/TaoQuickDemo_ubuntu_xenial_x64.AppImage
